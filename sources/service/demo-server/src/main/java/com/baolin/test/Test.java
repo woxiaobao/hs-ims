e85778a1f9825ad079ef8229c50b3c214e07dc7d
HS\lvbl28559 <574727356@qq.com>
2020-01-19 16:58:48 +0800

    增加test

---
+++ sources/service/demo-server/src/main/java/com/baolin/test/Test.java
@@ -0,0 +1,11 @@
+package com.baolin.test;
+
+/**
+ * @author lvbaolin
+ * @date 2020/1/19 16:57
+ */
+public class Test {
+    public static void main(String[] ager){
+        System.out.println("hello world!");
+    }
+}
